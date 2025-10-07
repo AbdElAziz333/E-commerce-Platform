ab6e6770813556f4a80080665389415ff74cf388
AbdElAziz <abdelaziz524aziz@gmail.com>
2025-10-07 10:40:38 +0300

    Create docker compose file

--- src/main/java/com/aziz/user_service/UserServiceApplication.java
+++ src/main/java/com/aziz/user_service/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.aziz.user_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./user-service").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 }
