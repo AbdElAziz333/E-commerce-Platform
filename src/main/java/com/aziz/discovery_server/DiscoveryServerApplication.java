39da4a3724628694f2b1d25527d23a01fa9b1fc1
AbdElAziz <abdelaziz524aziz@gmail.com>
2025-10-07 10:38:43 +0300

    Fetch token from .env

--- src/main/java/com/aziz/discovery_server/DiscoveryServerApplication.java
+++ src/main/java/com/aziz/discovery_server/DiscoveryServerApplication.java
@@ -1,5 +1,6 @@
 package com.aziz.discovery_server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,6 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServerApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./discovery-server").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 }
