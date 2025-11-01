0bb927e6040d950d24adaad476f94e8229c2a0a3
AbdElAziz <abdelaziz524aziz@gmail.com>
2025-11-01 21:18:23 +0200

    setup basic configuration for order service

--- src/main/java/com/aziz/order_service/OrderServiceApplication.java
+++ src/main/java/com/aziz/order_service/OrderServiceApplication.java
@@ -1,13 +1,15 @@
 package com.aziz.order_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./order-service").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
-
 }
