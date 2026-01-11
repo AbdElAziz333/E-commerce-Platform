9e4cd27b932358207f6358d0c22693269116bd8e
AbdElAziz <abdelaziz524aziz@gmail.com>
2026-01-11 15:28:51 +0200

    update: adjust docker compose and make and cassandra init script

--- cassandra/init/cassandra-init.sh
+++ cassandra/init/cassandra-init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Start Cassandra in background
-docker-entrypoint.sh cassandra -f &
+/usr/local/bin/docker-entrypoint.sh cassandra -f &
 
 # Wait for Cassandra to be ready
 echo "Waiting for Cassandra to start..."
-until cqlsh -e "describe cluster" > /dev/null 2>&1; do
+until cqlsh -u cassandra -p cassandra -e "describe cluster" > /dev/null 2>&1; do
   sleep 2
 done
 
 echo "Cassandra is up, creating keyspace..."
 
 # Create keyspace
-cqlsh -e "CREATE KEYSPACE IF NOT EXISTS notification WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
+cqlsh -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS ${CASSANDRA_KEYSPACE} WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};"
 
 echo "Keyspace created successfully!"
 
